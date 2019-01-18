5ad5328fac74cf019fc06aa06c3e3c83e72ee6ab
Joe Reynolds <jreynolds@likeabosscoding.com>
2019-01-18 15:12:40 -0600

    Initial work

---
+++ Samples/Samples/ConsoleAppCore/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleAppCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string foo = "foo";
+            // string notnull = null;
+            Console.WriteLine($"First Character of Foo is {foo[0]}");
+
+            string? fooNull = null;
+            if(!string.IsNullOrEmpty(fooNull))
+                Console.WriteLine($"First Character of Foo is {fooNull[0]}");
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
