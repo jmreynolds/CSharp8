5979365a6bad2ce00f31c5e24f0172c63db098b9
Joe Reynolds <jreynolds@likeabosscoding.com>
2019-04-12 15:33:32 -0500

    added pattern matching

---
+++ Samples/PatternMatching/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace PatternMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static RGBColor FromRainbowClassic(Rainbow colorBand)
+        {
+            switch (colorBand)
+            {
+                case Rainbow.Red:
+                    return new RGBColor(0xFF, 0x00, 0x00);
+                case Rainbow.Orange:
+                    return new RGBColor(0xFF, 0x7F, 0x00);
+                case Rainbow.Yellow:
+                    return new RGBColor(0xFF, 0xFF, 0x00);
+                case Rainbow.Green:
+                    return new RGBColor(0x00, 0xFF, 0x00);
+                case Rainbow.Blue:
+                    return new RGBColor(0x00, 0x00, 0xFF);
+                case Rainbow.Indigo:
+                    return new RGBColor(0x4B, 0x00, 0x82);
+                case Rainbow.Violet:
+                    return new RGBColor(0x94, 0x00, 0xD3);
+                default:
+                    throw new ArgumentException(message: "invalid enum value", 
+                        paramName: nameof(colorBand));
+            };
+        }
+
+        public static RGBColor FromRainbow(Rainbow colorBand) =>
+            colorBand switch
+            {
+                Rainbow.Red => new RGBColor(0xFF, 0x00, 0x00),
+                Rainbow.Orange => new RGBColor(0xFF, 0x7F, 0x00),
+                Rainbow.Yellow => new RGBColor(0xFF, 0xFF, 0x00),
+                Rainbow.Green => new RGBColor(0x00, 0xFF, 0x00),
+                Rainbow.Blue => new RGBColor(0x00, 0x00, 0xFF),
+                Rainbow.Indigo => new RGBColor(0x4B, 0x00, 0x82),
+                Rainbow.Violet => new RGBColor(0x94, 0x00, 0xD3),
+                _ => throw new ArgumentException(message: "invalid enum value", 
+                    paramName: nameof(colorBand)),
+            };
+
+        public static string RockPaperScissors(string first, string second) => 
+            (first, second) switch
+            {
+                ("rock", "paper") => "rock is covered by paper. Paper wins.",
+                ("rock", "scissors") => "rock breaks scissors. Rock wins.",
+                ("paper", "rock") => "paper covers rock. Paper wins.",
+                ("paper", "scissors") => "paper is cut by scissors. Scissors wins.",
+                ("scissors", "rock") => "scissors is broken by rock. Rock wins.",
+                ("scissors", "paper") => "scissors cuts paper. Scissors wins.",
+                (_, _) => "tie"
+            };
+    }
+
+    internal class RGBColor
+    {
+        private int Red;
+        private int Green;
+        private int Blue;
+
+        public RGBColor(int red, int green, int blue)
+        {
+            this.Red = red;
+            this.Green = green;
+            this.Blue = blue;
+        }
+    }
+
+    public enum Rainbow
+    {
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Indigo,
+        Violet
+    }
+}
