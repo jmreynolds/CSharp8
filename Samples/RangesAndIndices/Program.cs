abb0e0a8b5ada768c5a56d3a30c58a66c597a75f
Joe Reynolds <jreynolds@likeabosscoding.com>
2019-04-25 11:11:16 -0500

    fixed file name of staticlocalfunctions

--- Samples/RangesAndIndices/Program.cs
+++ Samples/RangesAndIndices/Program.cs
@@ -8,30 +8,42 @@ namespace RangesAndIndices
     {
         static void Main(string[] args)
         {
-            WriteLine("Names 1-3:");
-            foreach (var name in GetRangeOfNames(1..4))
+            foreach (var name in GetNames())
             {
-                // Endpoint of range is EXCLUSIVE
                 WriteLine(name);
             }
 
-            WriteLine("Names 1-3 - using end index:");
-            foreach (var name in GetRangeOfNames(1..^1))
+
+            var words = new string[]
             {
-                WriteLine(name);
-            }
+                            // index from start    index from end
+                "The",      // 0                   ^9
+                "quick",    // 1                   ^8
+                "brown",    // 2                   ^7
+                "fox",      // 3                   ^6
+                "jumped",   // 4                   ^5
+                "over",     // 5                   ^4
+                "the",      // 6                   ^3
+                "lazy",     // 7                   ^2
+                "dog"       // 8                   ^1
+            };
+            WriteLine($"The last word is: {words[^1]}");
         }
-        static string[] names =
+
+    static IEnumerable<string> GetNames()
+    {
+        string[] names =
         {
             "Archimedes", "Pythagoras", "Euclid", "Socrates", "Plato"
         };
-        static IEnumerable<string> GetRangeOfNames(Range range)
+        foreach (var name in names[1..4])
         {
-
-            foreach (var name in names[range])
-            {
-                yield return name;
-            }
+            yield return name;
         }
+    }
+
+
+
+
     }
 }
